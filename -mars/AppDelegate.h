22ad4a673819ad6fd4ea0fff028032f1e4fb9e47
hongxichen <hxc@hongxichendemacbook-pro.local>
2016-04-10 13:30:50 +0800

    Initial Commit

---
+++ -mars/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  -mars
+//
+//  Created by 洪曦尘 on 16/4/10.
+//  Copyright © 2016年 洪曦尘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ -mars/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  -mars
+//
+//  Created by 洪曦尘 on 16/4/10.
+//  Copyright © 2016年 洪曦尘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
